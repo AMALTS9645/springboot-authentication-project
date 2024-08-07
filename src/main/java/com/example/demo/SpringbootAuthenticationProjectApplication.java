a029b0f5924f70a786da6c196f47f3475e679554
Sinajmon <you@example.comsinajmon.kalathiparambilaboo@deliveryoperations.onmicrosoft.com>
2024-08-07 13:32:57 +0000

    new project

---
+++ src/main/java/com/example/demo/SpringbootAuthenticationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthenticationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthenticationProjectApplication.class, args);
+	}
+
+}
